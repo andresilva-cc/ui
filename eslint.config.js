ba5d4077c03d734d23db26dc366dcc54aa7531b9
André Silva <andreluiz.97@outlook.com>
2024-11-12 22:09:03 -0300

    chore: add typescript, eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  jsxA11y.flatConfigs.recommended,
+  prettier,
+];
